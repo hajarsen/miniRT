1c7e19c6907bd471377d04596a96799f6d6672ab
hajar.sen <hajarsennane1@gmail.com>
2025-12-28 20:15:25 +0100

    done

--- src/core/window.c
+++ src/core/window.c
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/28 20:10:40 by hsennane          #+#    #+#             */
+/*   Updated: 2025/12/28 20:10:41 by hsennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
-//window.c
+
 int	close_window(t_minirt *data)
 {
 	clean_scene(data->scene);
@@ -16,20 +28,19 @@ int	close_window(t_minirt *data)
 	return (0);
 }
 
-void put_pixel(t_img *img, int x, int y, int color)
+void	put_pixel(t_img *img, int x, int y, int color)
 {
-    char *dst;
-    
-    if (x < 0 || x >= img->width || y < 0 || y >= img->height)
-        return;
-    
-    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+	char	*dst;
+
+	if (x < 0 || x >= img->width || y < 0 || y >= img->height)
+		return ;
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
 int	key_hook(int keycode, t_minirt *data)
 {
-	if (keycode == XK_Escape || keycode == 65307) 
+	if (keycode == XK_Escape || keycode == 65307)
 		return (close_window(data));
 	return (0);
 }
@@ -39,25 +50,18 @@ int	init_window(t_minirt *data)
 	data->mlx = mlx_init();
 	if (!data->mlx)
 		return (parse_error("MiniLibX initialization failed"), 0);
-	
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "miniRT");
 	if (!data->win)
 		return (parse_error("Window creation failed"), 0);
-	
 	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	if (!data->img.img)
 		return (parse_error("Image creation failed"), 0);
-	
-	data->img.addr = mlx_get_data_addr(data->img.img,
-			&data->img.bits_per_pixel,
-			&data->img.line_length,
-			&data->img.endian);
+	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
+			&data->img.line_length, &data->img.endian);
 	data->img.width = WIDTH;
 	data->img.height = HEIGHT;
-	
 	mlx_hook(data->win, 2, 1L << 0, key_hook, data);
 	mlx_hook(data->win, 17, 0, close_window, data);
-	
 	return (1);
 }
 
