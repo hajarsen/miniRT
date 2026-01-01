5153df221028e1902fc1edf19e1eb83042685c7b
hajar.sen <hajarsennane1@gmail.com>
2026-01-01 14:09:58 +0100

    fix norm

--- src/core/window.c
+++ src/core/window.c
@@ -6,7 +6,7 @@
 /*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 20:10:40 by hsennane          #+#    #+#             */
-/*   Updated: 2026/01/01 13:56:23 by hsennane         ###   ########.fr       */
+/*   Updated: 2026/01/01 14:09:13 by hsennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int	init_window(t_minirt *data)
 		free(data->mlx);
 		return (parse_error("Window creation failed"), 0);
 	}
-		data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	if (!data->img.img)
 		return (parse_error("Image creation failed"), 0);
 	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
