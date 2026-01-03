4bd8ffdf4a415f8567eef8b824ffb518613134e6
hajar.sen <hajarsennane1@gmail.com>
2026-01-03 03:38:51 +0100

    added gnl

--- lib/get_next_line/get_next_line.c
+++ lib/get_next_line/get_next_line.c
@@ -1,35 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 21:05:24 by hsennane          #+#    #+#             */
+/*   Updated: 2026/01/03 03:37:30 by hsennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+static void	free_null(char **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
+}
+
+static char	*join_line(int nl_position, char **buffer)
+{
+	char	*res;
+	char	*tmp;
+
+	if (nl_position <= 0)
+	{
+		if (!*buffer || **buffer == '\0')
+		{
+			free_null(buffer);
+			return (NULL);
+		}
+		res = *buffer;
+		*buffer = NULL;
+		return (res);
+	}
+	tmp = ft_substr(*buffer, nl_position, ft_strlen(*buffer) - nl_position);
+	res = ft_substr(*buffer, 0, nl_position);
+	free_null(buffer);
+	*buffer = tmp;
+	return (res);
+}
+
+static char	*read_line(int fd, char **buffer, char *read_return)
+{
+	ssize_t	bytes_read;
+	char	*tmp;
+	char	*nl;
+
+	nl = ft_strchr(*buffer, '\n');
+	while (nl == NULL)
+	{
+		bytes_read = read(fd, read_return, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free_null(buffer);
+			return (NULL);
+		}
+		if (bytes_read == 0)
+			return (join_line(bytes_read, buffer));
+		read_return[bytes_read] = '\0';
+		tmp = ft_strjoin(*buffer, read_return);
+		free_null(buffer);
+		if (!tmp)
+			return (NULL);
+		*buffer = tmp;
+		nl = ft_strchr(*buffer, '\n');
+	}
+	return (join_line(nl - *buffer + 1, buffer));
+}
 
 char	*get_next_line(int fd)
 {
-	static int	position = 0;
-	static int	nbr_read = 0;
-	static char	buffer[BUFFER_SIZE];
-	char		line[MAX_LINE_SIZE];
-	int			i;
+	static char	*buffer;
+	char		*res;
+	char		*read_return;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	i = 0;
-	while (1)
+	read_return = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!read_return)
+		return (NULL);
+	if (!buffer)
 	{
-		if (position >= nbr_read)
+		buffer = ft_strdup("");
+		if (!buffer)
 		{
-			nbr_read = read(fd, buffer, BUFFER_SIZE);
-			position = 0;
-			if (nbr_read <= 0)
-				break ;
+			free(read_return);
+			return (NULL);
 		}
-		line[i] = buffer[position];
-		position++;
-		i++;
-		if (buffer[position - 1] == '\n') 
-			break ;
-		if (i >= MAX_LINE_SIZE - 1) 
-			break ;
 	}
-	line[i] = '\0';
-	if (i == 0)
-		return (NULL);
-	return (ft_strdup(line));
+	res = read_line(fd, &buffer, read_return);
+	free(read_return);
+	return (res);
 }
--- lib/get_next_line/get_next_line.h
+++ lib/get_next_line/get_next_line.h
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 21:07:41 by hsennane          #+#    #+#             */
+/*   Updated: 2026/01/03 03:32:50 by hsennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 # include <stdlib.h>
 # include <unistd.h>
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1024
 # endif
 
-# define MAX_LINE_SIZE 1000000
-
 char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int n);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *str);
-
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
